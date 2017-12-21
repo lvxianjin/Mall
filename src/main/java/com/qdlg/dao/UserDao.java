1787e56a75ba1a9b4063ab205e922750bedde569
lvxianjin <1018452609@qq.com>
2017-12-21 15:37:45 +0800

    更新框架

--- src/main/java/com/qdlg/dao/UserDao.java
+++ src/main/java/com/qdlg/dao/UserDao.java
@@ -1,7 +1,12 @@
 package com.qdlg.dao;
 
+import com.qdlg.model.User;
+
+import java.util.Map;
+
 /**
  * Created by 10184 on 2017/12/20.
  */
-public class UserDao {
+public interface UserDao {
+    Map<String,String> checkUser(Map<String,Object> dto)throws Exception;
 }
